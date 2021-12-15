83a8832c8c71653a05aae3e25cfa89b524a538ee
Rinat Valiev <valiev1379@gmail.com>
2021-12-15 17:13:38 +0300

    learner

---
+++ week_2/learner.cpp
@@ -0,0 +1,61 @@
+#include <algorithm>
+#include <string>
+#include <vector>
+#include <set>
+#include <sstream>
+
+#include "profile.h"
+#include "test_runner.h"
+
+using namespace std;
+
+class Learner {
+private:
+    set<string> dict;
+
+public:
+    int Learn(const vector<string> &words) {
+        set<string> temp(words.begin(), words.end());
+
+        int size = static_cast<int>(temp.size());
+
+        dict.merge(temp);
+
+//        cout << dict << endl << temp << endl;
+
+        return size - static_cast<int>(temp.size());
+    }
+
+    vector<string> KnownWords() {
+        return vector<string>{dict.begin(), dict.end()};
+    }
+};
+
+int main() {
+    Learner learner;
+    string line;
+
+    int i = 0;
+
+    while (getline(cin, line)) {
+        vector<string> words;
+        stringstream ss(line);
+        string word;
+
+        while (ss >> word) {
+            words.push_back(word);
+        }
+
+        cout << learner.Learn(words) << "\n";
+
+        if (i++ > 1) break;
+    }
+
+    cout << "=== known words ===\n";
+
+    for (const auto &word: learner.KnownWords()) {
+        cout << word << "\n";
+    }
+
+    return 0;
+}
