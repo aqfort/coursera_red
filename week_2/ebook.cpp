e7c870720dcff7b8be170ce4d07225b02191af1e
Rinat Valiev <valiev1379@gmail.com>
2022-01-04 21:41:02 +0500

    contest +

--- week_2/ebook.cpp
+++ week_2/ebook.cpp
@@ -10,6 +10,40 @@ using namespace std;
 
 class ReadingManager {
 private:
+    static const int MAX_ID = 100'000;
+    static const int MAX_PAGE = 1'000;
+
+    vector<int> _id;
+    vector<int> _page;
+
+public:
+    ReadingManager() :
+            _id(MAX_ID + 1, -1),
+            _page(MAX_PAGE + 1, 0) {}
+
+    void Read(const int & id, const int& page) {
+        for (int i = _id[id] + 1; i <= page; i++) {
+            _page[i]++;
+        }
+
+        _id[id] = page;
+    }
+
+    [[nodiscard]] double Cheer(const int& id) const {
+        if (_id[id] == -1) {
+            return 0.0;
+        }
+
+        if (_page[0] == 1) {
+            return 1.0;
+        }
+
+        return static_cast<double>(_page[0] - _page[_id[id]]) / static_cast<double>(_page[0] - 1);
+    }
+};
+
+class ReadingManagerStillSlow {
+private:
     struct Person {
         int _id;
         int _page;
