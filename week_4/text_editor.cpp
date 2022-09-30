5a7086724157bb4eb36f2d46eb57ed7582de85f4
aqfort <aqfort.cpp@gmail.com>
2022-09-30 17:11:49 +0300

    my_sol

---
+++ week_4/text_editor.cpp
@@ -0,0 +1,202 @@
+#include "test_runner.h"
+#include "profile.h"
+
+#include <string>
+#include <list>
+
+using namespace std;
+
+class Editor
+{
+public:
+    // Реализуйте конструктор по умолчанию и объявленные методы
+    Editor();
+    void Left();
+    void Right();
+    void Insert(char token);
+    void Cut(size_t tokens = 1);
+    void Copy(size_t tokens = 1);
+    void Paste();
+    string GetText() const;
+
+private:
+    list<char> text;
+    list<char> buffer;
+    list<char>::iterator cursor;
+};
+
+Editor::Editor() : cursor(text.end()) {}
+
+void Editor::Left()
+{
+    if (cursor != text.begin())
+    {
+        cursor--;
+    }
+}
+
+void Editor::Right()
+{
+    if (cursor != text.end())
+    {
+        cursor++;
+    }
+}
+
+void Editor::Insert(char token)
+{
+    text.insert(cursor, token);
+}
+
+void Editor::Cut(size_t tokens)
+{
+    list<char>::iterator temp = cursor;
+    for (size_t i = tokens; i > 0; i--)
+    {
+        temp++;
+
+        if (temp == text.end())
+        {
+            break;
+        }
+    }
+
+    buffer.assign(cursor, temp);
+    cursor = text.erase(cursor, temp);
+}
+
+void Editor::Copy(size_t tokens)
+{
+    list<char>::iterator temp = cursor;
+    for (size_t i = tokens; i > 0; i--)
+    {
+        temp++;
+
+        if (temp == text.end())
+        {
+            break;
+        }
+    }
+
+    buffer.assign(cursor, temp);
+}
+
+void Editor::Paste()
+{
+    text.insert(cursor, buffer.begin(), buffer.end());
+}
+
+string Editor::GetText() const
+{
+    return {text.begin(), text.end()};
+}
+
+// --- --- --- --- --- --- --- --- ---
+
+void TypeText(Editor &editor, const string &text)
+{
+    for (char c : text)
+    {
+        editor.Insert(c);
+    }
+}
+
+void TestEditing()
+{
+    {
+        Editor editor;
+
+        const size_t text_len = 12;
+        const size_t first_part_len = 7;
+        TypeText(editor, "hello, world");
+        for (size_t i = 0; i < text_len; ++i)
+        {
+            editor.Left();
+        }
+        editor.Cut(first_part_len);
+        for (size_t i = 0; i < text_len - first_part_len; ++i)
+        {
+            editor.Right();
+        }
+        TypeText(editor, ", ");
+        editor.Paste();
+        editor.Left();
+        editor.Left();
+        editor.Cut(3);
+
+        ASSERT_EQUAL(editor.GetText(), "world, hello");
+    }
+    {
+        Editor editor;
+
+        TypeText(editor, "misprnit");
+        editor.Left();
+        editor.Left();
+        editor.Left();
+        editor.Cut(1);
+        editor.Right();
+        editor.Paste();
+
+        ASSERT_EQUAL(editor.GetText(), "misprint");
+    }
+}
+
+void TestReverse()
+{
+    Editor editor;
+
+    const string text = "esreveR";
+    for (char c : text)
+    {
+        editor.Insert(c);
+        editor.Left();
+    }
+
+    ASSERT_EQUAL(editor.GetText(), "Reverse");
+}
+
+void TestNoText()
+{
+    Editor editor;
+    ASSERT_EQUAL(editor.GetText(), "");
+
+    editor.Left();
+    editor.Left();
+    editor.Right();
+    editor.Right();
+    editor.Copy(0);
+    editor.Cut(0);
+    editor.Paste();
+
+    ASSERT_EQUAL(editor.GetText(), "");
+}
+
+void TestEmptyBuffer()
+{
+    Editor editor;
+
+    editor.Paste();
+    TypeText(editor, "example");
+    editor.Left();
+    editor.Left();
+    editor.Paste();
+    editor.Right();
+    editor.Paste();
+    editor.Copy(0);
+    editor.Paste();
+    editor.Left();
+    editor.Cut(0);
+    editor.Paste();
+
+    ASSERT_EQUAL(editor.GetText(), "example");
+}
+
+int main()
+{
+    TestRunner tr;
+    RUN_TEST(tr, TestEditing);
+    RUN_TEST(tr, TestReverse);
+    RUN_TEST(tr, TestNoText);
+    RUN_TEST(tr, TestEmptyBuffer);
+    return 0;
+}
