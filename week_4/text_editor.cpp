cbae2790f432ae2f2cf4004c3e387524fcd9061a
aqfort <aqfort.cpp@gmail.com>
2022-09-30 17:17:25 +0300

    text

--- week_4/text_editor.cpp
+++ week_4/text_editor.cpp
@@ -93,6 +93,64 @@ string Editor::GetText() const
 
 // --- --- --- --- --- --- --- --- ---
 
+// class Editor {
+// public:
+//   Editor()
+//     : pos(text.end()) {
+//   }
+
+//   void Left() {
+//     pos = Advance(pos, -1);
+//   }
+
+//   void Right() {
+//     pos = Advance(pos, 1);
+//   }
+
+//   void Insert(char token) {
+//     text.insert(pos, token);
+//   }
+
+//   void Cut(size_t tokens = 1) {
+//     auto pos2 = Advance(pos, tokens);
+//     buffer.assign(pos, pos2);
+//     pos = text.erase(pos, pos2);
+//   }
+
+//   void Copy(size_t tokens = 1) {
+//     auto pos2 = Advance(pos, tokens);
+//     buffer.assign(pos, pos2);
+//   }
+
+//   void Paste() {
+//     text.insert(pos, buffer.begin(), buffer.end());
+//   }
+
+//   string GetText() const {
+//     return {text.begin(), text.end()};
+//   }
+
+// private:
+//   using Iterator = list<char>::iterator;
+//   list<char> text;
+//   list<char> buffer;
+//   Iterator pos;
+
+//   Iterator Advance(Iterator it, int steps) const {
+//     while (steps > 0 && it != text.end()) {
+//       ++it;
+//       --steps;
+//     }
+//     while (steps < 0 && it != text.begin()) {
+//       --it;
+//       ++steps;
+//     }
+//     return it;
+//   }
+// };
+
+// --- --- --- --- --- --- --- --- ---
+
 void TypeText(Editor &editor, const string &text)
 {
     for (char c : text)
